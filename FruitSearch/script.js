27454bf515aedb2b138eca195a212828392a27b1
nichen2 <nickychen434@gmail.com>
2023-09-09 00:39:28 -0700

    connect4 starter code

--- FruitSearch/script.js
+++ FruitSearch/script.js
@@ -85,52 +85,48 @@ const fruits = [
 function search(str) {
   let results = [];
   let count = 0;
-  fruits.forEach(fruit => {
-	let lower = fruit.toLowerCase();
-	if (lower.includes(str)) {
-		if (count === 10) return results;
-		count += 1;
-		results.push(fruit);
-	}
-  }); 
+  fruits.forEach((fruit) => {
+    let lower = fruit.toLowerCase();
+    if (lower.includes(str)) {
+      if (count === 10) return results;
+      count += 1;
+      results.push(fruit);
+    }
+  });
   return results;
 }
 
+/* Clear suggestions first in order to properly show modified suggestions
+   after new keyup */
 function searchHandler(e) {
   let inputVal = input.value.toLowerCase();
-  // console.log(inputVal);
   const results = search(inputVal);
-  // console.log(results);
   clearSuggestions(suggestions);
   showSuggestions(results, inputVal);
   if (inputVal == "" || inputVal == null) {
-	clearSuggestions(suggestions);
+    clearSuggestions(suggestions);
   }
 }
 
 function showSuggestions(results, inputVal) {
-  results.forEach(result => {
-	let suggestion = document.createElement('li');
-  	suggestion.innerText = result;
-	suggestions.appendChild(suggestion);
+  results.forEach((result) => {
+    let suggestion = document.createElement("li");
+    suggestion.innerText = result;
+    suggestions.appendChild(suggestion);
   });
 }
 
 function useSuggestion(e) {
-  // TODO
-  // console.log(e.target.innerText);
   let suggestion = e.target.innerText;
   input.value = suggestion;
   clearSuggestions(suggestions);
 }
 
 function clearSuggestions(suggestions) {
-  while(suggestions.firstChild) {
-	suggestions.removeChild(suggestions.firstChild);
+  while (suggestions.firstChild) {
+    suggestions.removeChild(suggestions.firstChild);
   }
 }
 
 input.addEventListener("keyup", searchHandler);
 suggestions.addEventListener("click", useSuggestion);
-
-
