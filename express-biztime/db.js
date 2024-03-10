1c927e232a20bf8bf2c3724c682936837f896454
nichen2 <nickychen434@gmail.com>
2024-03-09 19:34:17 -0800

    added env file

---
+++ express-biztime/db.js
@@ -0,0 +1,24 @@
+/** Database setup for BizTime. */
+
+const { Client } = require("pg");
+require('dotenv').config();
+
+let dbName;
+
+if (process.env.NODE_ENV === "test") {
+  dbName = 'biztime_test'
+} else {
+  dbName = "biztime";
+}
+
+let db = new Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: dbName
+});
+
+db.connect();
+
+module.exports = db;
