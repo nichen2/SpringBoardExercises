a24b2ba5c3b24db95bbdefcc0ccdd9cc553c58ed
Nicky Chen <nickychen@Nickys-MacBook-Pro.local>
2024-05-01 07:14:07 -0700

    finished binary trees exercise

--- dsa-trees/binary-tree.js
+++ dsa-trees/binary-tree.js
@@ -16,29 +16,74 @@ class BinaryTree {
   /** minDepth(): return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
 
-  minDepth() {
-
+  minDepth(node = this.root) {
+    if (!node) {
+      return 0;
+    }
+    const queue = [node];
+    let depth = 1;
+    while (queue.length > 0) {
+      const currNode = queue.shift();
+      if (!currNode.left && !currNode.right) return depth;
+      if (currNode.left !== null) queue.push(currNode.left);
+      if (currNode.right !== null) queue.push(currNode.right);
+      depth += 1;
+    } 
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
-  maxDepth() {
-
+  maxDepth(root = this.root) {
+    if (!root) {
+      return 0;
+    }
+    const leftDepth = this.maxDepth(root.left);
+    const rightDepth = this.maxDepth(root.right);
+    return Math.max(leftDepth, rightDepth) + 1;
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
-  maxSum() {
+  maxSum(node = this.root) {
+    let max = -Infinity;
+    function maxGain(node) {
+      if (!node) {
+        max = 0;
+        return 0;
+      }
+      const leftGain = Math.max(maxGain(node.left),0);
+      const rightGain = Math.max(maxGain(node.right),0);
+      const currentPathVal = node.val + leftGain + rightGain;
+      max = Math.max(max, currentPathVal);
 
+      return node.val + Math.max(leftGain, rightGain);
+    }
+    maxGain(node);
+    return max;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
-  nextLarger(lowerBound) {
-
+  nextLarger(lowerBound, root = this.root) {
+    const vals = [];
+    fillVals = (node) => {
+      if (!node) return null;
+      const queue = [node];
+      while (queue.length > 0) {
+        const currVal = queue.shift();
+        vals.push(currVal.val);
+        if(currVal.left) queue.push(currVal.left);
+        if(currVal.right) queue.push(currVal.right);
+      }
+    }
+    fillVals(root);
+    const greaterThan = vals.filter(val => val > lowerBound);
+    if (greaterThan.length === 0) return null;
+    greaterThan.sort();
+    return greaterThan[0];
   }
 
   /** Further study!
